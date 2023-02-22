17cdd14950be686041088d7c430e05b7536cf473
RonaldAndero <ronald-andero.oisman@voco.ee>
2023-02-22 03:03:57 +0200

    WIP

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yamlJs = require('yamljs');
 const swaggerDocument = yamlJs.load('./swagger.yaml');
+const mysql = require('mysql')
 
 require('dotenv').config();
 
@@ -17,6 +18,20 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Middleware to parse JSON
 app.use(express.json());
 
+// create db connection
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "forum"
+})
+
+con.connect(function (err){
+    if (err) throw err
+    console.log("Connected to joga_mysql db");
+})
+
+
 const users = [{username: "admin", password: "admin", isAdmin: true},
     {username: "user", password: "password", isAdmin: false}]
 
@@ -24,6 +39,13 @@ const sessions = []
 
 
 app.post('/sessions', (req,res) => {
+    con.query("SELECT * FROM forum.accounts", (err, res) => {
+        if (err) throw err;
+        const row = res[0];
+        const username = row.username;
+        console.log(username)
+        console.log(req.body.username)
+    })
     if (!req.body.username || !req.body.password){
         return res.status(400).send({error: "One or more parameters missing"})
     } else {
@@ -49,6 +71,20 @@ app.post('/sessions', (req,res) => {
     }
 });
 
+app.post('/logout', (req, res) => {
+    if (!req.body.username || !req.body.sessionId){
+        return res.status(400).send({error: "One or more parameters missing"})
+    } else {
+        sessions.forEach((element) => {
+            if (element.user == req.body.username || element.id == req.body.sessionId) {
+                sessions.splice(element)
+                return res.status(201).send({success: true})
+            } else {
+                return res.status(401).send({error: "Invalid sessionId or username"})
+            }
+        })
+    }
+});
 // General error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
--- public/checkLogin.js
+++ public/checkLogin.js
@@ -1,6 +1,19 @@
 window.onload = checkSession();
 function checkSession() {
     if (localStorage.length >= 2) {
-        document.getElementById("login").style.display = "none";
+        document.getElementById("loginBtn").style.display = "none";
+        document.getElementById('logoutBtn').style.display = ""
+    }
+}
+
+function clearInputs() {
+    var inputs = document.getElementsByTagName('input');
+    var errorMessages = document.getElementById('s-error-msg');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value = '';
+    }
+    var errorMsg = document.getElementById('s-error-msg');
+    if (errorMsg) {
+        errorMsg.textContent = '';
     }
 }
