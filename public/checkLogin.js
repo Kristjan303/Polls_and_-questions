b76c8ff047f90d2fcb840597fd4596f97f99eb30
RonaldAndero <ronald-andero.oisman@voco.ee>
2023-02-26 22:01:43 +0200

    WIP. Improves navbars with dynamic login state detection

--- public/checkLogin.js
+++ public/checkLogin.js
@@ -2,7 +2,21 @@ window.onload = checkSession();
 function checkSession() {
     if (localStorage.length >= 2) {
         document.getElementById("loginBtn").style.display = "none";
+        document.getElementById("mainLoginBtn").style.display = "none";
         document.getElementById('logoutBtn').style.display = ""
+    }
+    // check if user is logged in
+    if (!localStorage.getItem("loggedIn")) {
+        // add event listener to each anchor tag inside mainbar and sidenav
+        document.querySelectorAll("nav#mainbar ul li a, nav#sidebar ul li a").forEach(function(element) {
+            element.addEventListener("click", function(event) {
+                // prevent default anchor tag behavior
+                event.preventDefault();
+                // open Bootstrap modal
+                var loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+                loginModal.show();
+            });
+        });
     }
 }
 
