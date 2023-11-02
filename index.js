29c7a358d08048e0b9593dca2c95ca39dac5758d
Kristjan-even <kristjan.puusepp@voco.ee>
2023-11-02 11:44:07 +0200

    profile able to update, change and modify for user needs.

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const swaggerDocument = yamlJs.load('./swagger.yaml');
 const mysql = require('mysql2')
 const bcrypt = require('bcrypt');
 const swaggerJSDoc = require('swagger-jsdoc'); // Import swagger-jsdoc
+const jwt = require('jsonwebtoken');
 
+const cors = require('cors');
+const {join} = require("path");
+
+// Use the cors middleware with default options
+app.use(cors());
 
 require('dotenv').config();
 
@@ -49,23 +55,26 @@ var con = mysql.createConnection({
     password: "qwerty",
     database: "polls"
 })
+// Create a route to serve the profile page
+
+
 
 con.connect(function (err){
     if (err) throw err
     console.log("Connected polls db");
 })
 
+const sessions = []; // Define the sessions array to store user sessions
 
-const sessions = []
 
 app.post('/sessions', (req, res) => {
     // Check if the request body contains both username and password
     if (!req.body.username || !req.body.password) {
         // If not, send an error response with status code 400
-        return res.status(400).send({ error: "One or more parameters missing" })
+        return res.status(400).send({ error: "One or more parameters missing" });
     } else {
         // If both username and password are present, query the database to find a matching user
-        con.query('SELECT password FROM polls.accounts WHERE username = ?', [req.body.username], function (error, results, fields) {
+        con.query('SELECT username, password FROM polls.accounts WHERE username = ?', [req.body.username], function (error, results, fields) {
             if (error) throw error;
 
             // Check if the query returned any results
@@ -74,23 +83,25 @@ app.post('/sessions', (req, res) => {
                 const hashedPassword = results[0].password;
                 if (bcrypt.compareSync(req.body.password, hashedPassword)) {
                     // If the passwords match, create a new session with a random session ID
-                    const sessionId = Math.round(Math.random() * 100000000)
-                    const session = { id: sessionId, user: req.body.username }
-                    sessions.push(session)
+                    const sessionId = Math.round(Math.random() * 100000000);
+                    const session = { id: sessionId, user: req.body.username };
+                    sessions.push(session);
 
                     // Send a success response with status code 201, indicating that the user is logged in
-                    return res.status(201).send({ success: true, loggedIn: true, sessionId: sessionId })
+                    return res.status(201).send({ success: true, loggedIn: true, sessionId: sessionId });
                 } else {
                     // If the passwords do not match, send an error response with status code 401, indicating that the login credentials are invalid
-                    return res.status(401).send({ error: "Invalid username or password" })
+                    return res.status(401).send({ error: "Invalid username or password" });
                 }
             } else {
                 // If the query did not return any results, send an error response with status code 401, indicating that the login credentials are invalid
-                return res.status(401).send({ error: "Invalid username or password" })
+                return res.status(401).send({ error: "Invalid username or password" });
             }
         });
     }
 });
+
+
 
 
 
@@ -139,28 +150,80 @@ app.post('/registration', (req, res) => {
 
 
 
+
 // This endpoint is for logging out a user. It expects a POST request with a JSON body containing the username and sessionId.
 app.post('/logout', (req, res) => {
     // Check if the required parameters are present in the request body.
-    if (!req.body.username || !req.body.sessionId){
+    if (!req.body.username || !req.body.sessionId) {
         // If any parameter is missing, return a 400 Bad Request response with an error message.
-        return res.status(400).send({error: "One or more parameters missing"})
+        return res.status(400).send({ error: "One or more parameters missing" });
     } else {
-        // If all required parameters are present, iterate over the sessions array to find a matching session.
-        sessions.forEach((element) => {
-            if (element.user == req.body.username || element.id == req.body.sessionId) {
-                // If a matching session is found, remove it from the sessions array and return a 201 Created response with a success message.
-                sessions.splice(element)
-                return res.status(201).send({success: true})
-            } else {
-                // If no matching session is found, return a 401 Unauthorized response with an error message.
-                return res.status(401).send({error: "Invalid sessionId or username"})
-            }
-        })
+        // You can skip session verification here and directly log out the user by removing the parameters from the sessions array.
+        const index = sessions.findIndex(element => element.user === req.body.username && element.id === req.body.sessionId);
+        if (index !== -1) {
+            // If a matching session is found, remove it from the sessions array and return a 201 Created response with a success message.
+            sessions.splice(index, 1);
+            return res.status(201).send({ success: true });
+        } else {
+            // You can still return a success response even if the session doesn't exist to allow users to "log out" without verifying the session.
+            // return res.status(401).send({ error: "Invalid sessionId or username" });
+            return res.status(201).send({ success: true });
+        }
     }
 });
 
 
+
+app.get('/profile', (req, res) => {
+    const profilePagePath = join(__dirname, 'public', 'profile.html'); // Get the full path to the HTML file
+    res.sendFile(profilePagePath); // Serve the HTML file
+});
+app.post('/save-profile', (req, res) => {
+    const { sessionId, username, bio, profile_pic } = req.body;
+
+    // Verify the session ID and username
+    const validSession = sessions.find(session => session.id == sessionId && session.user === username);
+
+    console.log('sessionId:', sessionId, typeof sessionId);
+    console.log('username:', username, typeof username);
+    console.log('validSession:', validSession, typeof validSession);
+    console.log(sessions);
+
+
+    if (!validSession) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+    }
+
+    // Update the user's profile in the database
+    con.query('UPDATE polls.accounts SET bio = ?, profile_pic = ? WHERE username = ?', [bio, profile_pic, username], (updateError, updateResults) => {
+        if (updateError) {
+            console.error(updateError);
+            res.status(500).json({ error: 'Error saving profile' });
+        } else {
+            res.status(200).json({ message: 'Profile saved successfully' });
+        }
+    });
+});
+
+app.get('/get-profile-picture', (req, res) => {
+    const username = req.query.username;
+
+    // Query the database to get the user's profile picture
+    con.query('SELECT profile_pic FROM polls.accounts WHERE username = ?', [username], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error fetching profile picture' });
+        } else {
+            if (results.length > 0) {
+                const profile_pic = results[0].profile_pic;
+                res.status(200).json({ profile_pic });
+            } else {
+                res.status(404).json({ profile_pic: null });
+            }
+        }
+    });
+});
 
 // General error handler
 app.use((err, req, res, next) => {
